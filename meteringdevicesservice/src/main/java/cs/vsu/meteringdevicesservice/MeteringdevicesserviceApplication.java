075005a3a5a25226376b6fcd61ab70d420a16a84
Dmitry <dmitry.mikhalev@datavirtuality.de>
2021-03-07 17:23:27 +0300

    MDS-1 Create initial spring-boot application

---
+++ meteringdevicesservice/src/main/java/cs/vsu/meteringdevicesservice/MeteringdevicesserviceApplication.java
@@ -0,0 +1,13 @@
+package cs.vsu.meteringdevicesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteringdevicesserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteringdevicesserviceApplication.class, args);
+	}
+
+}
---
+++ meteringdevicesservice/src/test/java/cs/vsu/meteringdevicesservice/MeteringdevicesserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.vsu.meteringdevicesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteringdevicesserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
