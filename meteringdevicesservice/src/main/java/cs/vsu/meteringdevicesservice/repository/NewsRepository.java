a83621e6361d78886dc9b613639f5f76d0cd8a3f
dmikhalev <mikhalev.dmitrii.2000@mail.ru>
2021-05-07 15:45:33 +0300

    MDS-15 Add news

--- meteringdevicesservice/src/main/java/cs/vsu/meteringdevicesservice/repository/NewsRepository.java
+++ meteringdevicesservice/src/main/java/cs/vsu/meteringdevicesservice/repository/NewsRepository.java
@@ -3,5 +3,10 @@ package cs.vsu.meteringdevicesservice.repository;
 import cs.vsu.meteringdevicesservice.entity.News;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface NewsRepository extends JpaRepository<News, Long> {
+
+    List<News> findNewsByDateAfter(Date date);
 }
--- meteringdevicesservice/src/main/java/cs/vsu/meteringdevicesservice/service/NewsService.java
+++ meteringdevicesservice/src/main/java/cs/vsu/meteringdevicesservice/service/NewsService.java
@@ -5,15 +5,22 @@ import cs.vsu.meteringdevicesservice.exception.NotFoundException;
 import cs.vsu.meteringdevicesservice.repository.NewsRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 @Service
 public class NewsService {
+
+    private static final long MILLIS_IN_DAY = 24 * 60 * 60 * 1000;
+
     private final NewsRepository newsRepository;
 
     public NewsService(NewsRepository newsRepository) {
         this.newsRepository = newsRepository;
     }
 
-    public News findById(Long id) {
+    public News findById(Long id) throws NotFoundException {
         return newsRepository.findById(id).orElseThrow(NotFoundException::new);
     }
 
@@ -23,5 +30,10 @@ public class NewsService {
 
     public void delete(Long id) {
         newsRepository.deleteById(id);
+    }
+
+    public List<News> getNewsForTheLastDays(Integer days) {
+        Date date = new Date(System.currentTimeMillis() - (days * MILLIS_IN_DAY));
+        return new ArrayList<>(newsRepository.findNewsByDateAfter(date));
     }
 }
