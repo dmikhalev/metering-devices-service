27c92c9fc7956fe0bc032bb3d43202ab07c7d11f
dmikhalev <mikhalev.dmitrii.2000@mail.ru>
2021-05-07 15:38:59 +0300

    MDS-11 Add authentication

---
+++ meteringdevicesservice/src/main/java/cs/vsu/meteringdevicesservice/security/jwt/JwtUserFactory.java
@@ -0,0 +1,34 @@
+package cs.vsu.meteringdevicesservice.security.jwt;
+
+import cs.vsu.meteringdevicesservice.entity.Role;
+import cs.vsu.meteringdevicesservice.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public final class JwtUserFactory {
+
+    public JwtUserFactory() {
+    }
+
+    public static JwtUser create(User user) {
+        return new JwtUser(
+                user.getId(),
+                user.getLogin(),
+                user.getPassword(),
+                user.getName(),
+                user.getPhoneNumber(),
+                mapToGrantedAuthorities(Collections.singletonList(user.getRole()))
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Role> userRoles) {
+        return userRoles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+}
